859f00a7694ad7db65942d99e054a2fbc714176e
Sethuram-7 <83540194+Sethuram-7@users.noreply.github.com>
2024-04-14 21:51:30 +0530

    Update SeventeenthTestScript.java

--- SeventeenthTestScript.java
+++ SeventeenthTestScript.java
@@ -15,15 +15,14 @@ public class SeventeenthTestScript {
         Map<String, Object> prefs = new HashMap<String, Object>();
         prefs.put("download.default_directory", "C:\\Users\\Sethuram\\Downloads\\seleniumfiles");
         capabilities.setExperimentalOption("prefs", prefs);
-
         WebDriver driver = new ChromeDriver(capabilities);
         driver.get("https://www.tutorialspoint.com/selenium/practice/upload-download.php");
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         WebElement button_1 = driver.findElement(By.xpath("//*[@id=\"downloadButton\"]"));
         button_1.click();
         Thread.sleep(5000);
-       /* WebElement element = driver.findElement(By.xpath("//*[@id=\"uploadFile\"]"));
+        WebElement element = driver.findElement(By.xpath("//*[@id=\"uploadFile\"]"));
         element.sendKeys("C:\\Users\\Sethuram\\Downloads\\sampleFile.jpeg");
-*/
+        driver.quit();
     }
 }
