5604a0789e5822c5059bdb70dd1dc4b59e03123c
Sethuram-7 <83540194+Sethuram-7@users.noreply.github.com>
2024-04-15 22:14:40 +0530

    Update FirstTestScript.java

--- FirstTestScript.java
+++ FirstTestScript.java
@@ -9,17 +9,11 @@ import java.util.concurrent.TimeUnit;
 
 public class FirstTestScript {
     public static void main(String[] args) throws InterruptedException {
-
         WebDriver driver = new ChromeDriver();
-
         driver.get("https://www.tutorialspoint.com/selenium/practice/text-box.php");
-      String current_url =  driver.getCurrentUrl();
+        String current_url =  driver.getCurrentUrl();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-
-
         WebElement e = driver.findElement(By.xpath("//*[@id=\"fullname\"]"));
-
-
         e.sendKeys("Selenium");
         String text = driver.findElement(By.xpath("//*[@id=\"fullname\"]")).getAttribute("value");
         System.out.println( "text 1 = "+text);
