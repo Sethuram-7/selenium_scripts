3d0f108af1f77c2a8c23106547ef696fb2387084
Sethuram-7 <83540194+Sethuram-7@users.noreply.github.com>
2024-04-16 21:48:17 +0530

    Add files via upload

--- SixteenthTestScript.java
+++ SixteenthTestScript.java
@@ -1,10 +1,8 @@
 package org.example;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.concurrent.TimeUnit;
 
 public class SixteenthTestScript {
