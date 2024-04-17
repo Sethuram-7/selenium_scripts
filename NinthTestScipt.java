9a073e3bc1a8993520fc1abd7a8a0fcf1263031f
Sethuram-7 <83540194+Sethuram-7@users.noreply.github.com>
2024-04-17 19:34:46 +0530

    Add files via upload

--- NinthTestScipt.java
+++ NinthTestScipt.java
@@ -1,15 +1,12 @@
 package org.example;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.concurrent.TimeUnit;
 
 public class NinthTestScipt {
     public static void main(String[] args) throws InterruptedException {
-
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.tutorialspoint.com/selenium/practice/text-box.php");
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
