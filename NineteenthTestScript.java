88ac89b8ad1a0fc7a0633c412589357384244c03
Sethuram-7 <83540194+Sethuram-7@users.noreply.github.com>
2024-04-17 19:35:34 +0530

    Add files via upload

--- NineteenthTestScript.java
+++ NineteenthTestScript.java
@@ -1,16 +1,13 @@
 package org.example;
-
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.concurrent.TimeUnit;
 
 public class NineteenthTestScript {
     public static void main(String[] args) throws InterruptedException {
-
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.tutorialspoint.com/selenium/practice/alerts.php");
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
