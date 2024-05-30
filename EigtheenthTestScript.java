d4ab38820fcc8712e3190057ce814fca2a4e34ec
Sethuram-7 <83540194+Sethuram-7@users.noreply.github.com>
2024-05-30 22:15:29 +0530

    Add files via upload

--- EigtheenthTestScript.java
+++ EigtheenthTestScript.java
@@ -1,15 +1,12 @@
 package org.example;
-
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-
 public class EigtheenthTestScript {
     public static void main(String[] args) throws InterruptedException {
-
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.tutorialspoint.com/selenium/practice/browser-windows.php");
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
@@ -20,7 +17,6 @@ public class EigtheenthTestScript {
         driver.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL + "2");
         ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
         driver.switchTo().window(tabs.get(1));
-
         Thread.sleep(10000);
         WebElement Newtab_1 = driver.findElement(By.xpath("/html/body/main/div/div/h1"));
         System.out.println(Newtab_1.getText());
